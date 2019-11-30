95633cdb7dad1fb6a96f2f652b2c8600c6197e21
Qisas Tazkia Hasanudin <47523892+qisashasanudin@users.noreply.github.com>
2019-11-30 09:50:54 +0700

    v1.2
    - Kontrol WASD & mouse sudah diperbaiki
    - penambahan fitur "jump" & "crouch" (masih jelek, gravitasi belum diterapkan)
    - penambahan mode "spectator" (bergerak secara bebas ke sumbu x, y, maupun z)

--- main.c
+++ main.c
@@ -6,33 +6,92 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 
-char title[] = "Saya sedih sekali";
-float anglePyramid = 0.0f;  // Rotational angle for pyramid [NEW]
-float angleCube = 0.0f;     // Rotational angle for cube [NEW]
-int refreshRate = 1000/60;        // refresh interval in milliseconds [NEW]
+#define ENTER 13
+#define ESC 27
+#define SPACEBAR 32
 
+//===========================================================================================================================
+
+char title[] = "Saya sedih sekali";
+int res_x = 1366;
+int res_y = 768;
+float fov = 60.0f;
+int is_fullscreen = 0;
+int refreshRate = 1000/60;
+// WASD controls
 int up = 0;
 int down = 0;
 int left = 0;
 int right = 0;
-
+int jump = 0;
+int crouch = 0;
+int sprint = 0;
+int flying = 0;
 // angle of rotation for the camera direction
-float angle_x=0.0f;
-float angle_y=0.0f;
-// actual vector representing the camera's direction
-float lx=0.0f, ly=0.0f, lz=-1.0f;
-// current position of the camera
-float x=0.0f, y=1.0f, z=5.0f;
-
+int xOrigin = 683;
+int yOrigin = 384;
+float angle_x = 0.0f;
+float angle_y = 0.0f;
 float deltaAngle_x = 0.0f;
 float deltaAngle_y = 0.0f;
-float deltaMove_fwd = 0.0f;
-float deltaMove_side = 0.0f;
-
-int xMouseState = -1;
-int yMouseState = -1;
+// current position of the camera
+float x=0.0f, y=1.8f, z=5.0f;
+// actual vector representing the camera's direction
+float lx=0.0f, ly=0.0f, lz=-1.0f;
 
 //===========================================================================================================================
+
+void screenResize(int w, int h);
+void toggle_fullscreen();
+void render3D(void);
+void drawSnowMan();
+void key_press(unsigned char key, int xx, int yy);
+void key_release(unsigned char key, int x, int y);
+void specKey_press(int key, int xx, int yy);
+void specKey_release(int key, int x, int y);
+void move();
+void camera(int x, int y);
+void mouseButton(int button, int state, int x, int y);
+
+//===========================================================================================================================
+
+int main(int argc, char **argv) {
+
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(res_x,res_y);
+	glutCreateWindow(title);
+	glutSetCursor(GLUT_CURSOR_NONE);
+
+	// register callbacks
+	glutDisplayFunc(render3D);
+	glutReshapeFunc(screenResize);
+	glutIdleFunc(render3D);
+
+	//keyboard input
+	glutIgnoreKeyRepeat(1);
+	glutKeyboardFunc(key_press);
+	glutKeyboardUpFunc(key_release);
+	glutSpecialFunc(specKey_press);
+	glutSpecialUpFunc(specKey_release);
+
+	// camera & mouse input
+	glutPassiveMotionFunc(camera);
+	glutMouseFunc(mouseButton);
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	return 1;
+}
+
+//===========================================================================================================================
+
 
 void screenResize(int w, int h) {
 	if (h == 0)
@@ -42,9 +101,53 @@ void screenResize(int w, int h) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glViewport(0, 0, w, h);
-	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+	gluPerspective(fov, ratio, 0.1f, 1000.0f);
 	glMatrixMode(GL_MODELVIEW);
 }
+
+void toggle_fullscreen() {
+    if(!is_fullscreen){
+        glutFullScreen();
+        is_fullscreen = 1;
+    } else if(is_fullscreen){
+        glutReshapeWindow(res_x, res_y);
+        glutPositionWindow(100,100);
+        is_fullscreen = 0;
+    }
+}
+
+void render3D(void) {
+	glutWarpPointer(res_x/2, res_y/2);
+	move();
+	// Clear Color and Depth Buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Reset transformations
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(	x, y, z,
+			x+lx, y+ly, z+lz,
+			0.0f, 1.0f, 0.0f);
+			
+// Draw ground
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glBegin(GL_QUADS);
+		glVertex3f(-100.0f, 0.0f, -100.0f);
+		glVertex3f(-100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f, -100.0f);
+	glEnd();
+
+// Draw 36 SnowMen
+	int i,j;
+	for(i = -3; i < 3; i++)
+		for(j=-3; j < 3; j++) {
+                     glPushMatrix();
+                     glTranslatef(i*10.0,0,j * 10.0);
+                     drawSnowMan();
+                     glPopMatrix();
+               }
+        glutSwapBuffers();
+} 
 
 void drawSnowMan() {
 	glColor3f(1.0f, 1.0f, 1.0f);
@@ -68,184 +171,138 @@ void drawSnowMan() {
 	glutSolidCone(0.08f,0.5f,10,2);
 }
 
-void walk() {
-	x += deltaMove_fwd * lx * 0.1f;
-	z += deltaMove_fwd * lz * 0.1f;
-}
-
-void strafe() {
-	z += deltaMove_side * lx * 0.1f;
-	x += deltaMove_side * lz * 0.1f;
-}
-
-void renderScene(void) {
-	if (deltaMove_fwd)
-		walk();
-	if (deltaMove_side)
-		strafe();
-	// Clear Color and Depth Buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Reset transformations
-	glLoadIdentity();
-	// Set the camera
-	gluLookAt(	x, y, z,
-			x+lx, y+ly, z+lz,
-			0.0f, 1.0f, 0.0f);
-		printf("%f %f %f\n", x, y, z);
-			
-// Draw ground
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glBegin(GL_QUADS);
-		glVertex3f(-100.0f, 0.0f, -100.0f);
-		glVertex3f(-100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f, -100.0f);
-	glEnd();
-
-// Draw 36 SnowMen
-	int i,j;
-	for(i = -3; i < 3; i++)
-		for(j=-3; j < 3; j++) {
-                     glPushMatrix();
-                     glTranslatef(i*10.0,0,j * 10.0);
-                     drawSnowMan();
-                     glPopMatrix();
-               }
-        glutSwapBuffers();
-} 
-
-void key_press(unsigned char key, int xx, int yy) { 	
+void key_press(unsigned char key, int xx, int yy) {
+	int mod = glutGetModifiers();
+	if (mod == GLUT_ACTIVE_ALT){
+		if (key == ENTER){
+			toggle_fullscreen();
+		}
+	}
+	mod = 0;
     switch(key){
-    	case 27:
+    	case ESC:
     		exit(0);
     		break;
 		case 'w':
 			up = 1;
-			deltaMove_fwd = 0.5f;
 			break;
 		case 'a':
 			left = 1;
-			deltaMove_side = 0.5f;
 			break;
 		case 's':
 			down = 1;
-			deltaMove_fwd = -0.5f;
 			break;
 		case 'd':
 			right = 1;
-			deltaMove_side = -0.5f;
+			break;
+		case 'c':
+			crouch = 1;
+			break;
+		case SPACEBAR:
+			jump = 1;
 			break;
 	}
 }
 
-void key_release(unsigned char key, int x, int y)
-{
+void key_release(unsigned char key, int x, int y){
 	switch(key){
 		case 'w':
 			up = 0;
-			deltaMove_fwd = 0.0f;
+			sprint = 1;
 			break;
 		case 'a':
 			left = 0;
-			deltaMove_side = 0.0f;
 			break;
 		case 's':
 			down = 0;
-			deltaMove_fwd = 0.0f;
 			break;
 		case 'd':
 			right = 0;
-			deltaMove_side = 0.0f;
+			break;
+		case 'c':
+			crouch = 0;
 			break;
 	}
 }
 
 void specKey_press(int key, int xx, int yy) {
 
-       switch (key) {
-             case GLUT_KEY_UP : deltaMove_fwd = 0.5f; break;
-             case GLUT_KEY_DOWN : deltaMove_fwd = -0.5f; break;
-       }
+    switch (key) {
+    	
+    }
 } 
 
 void specKey_release(int key, int x, int y) { 	
 
-        switch (key) {
-             case GLUT_KEY_UP :
-             case GLUT_KEY_DOWN : deltaMove_fwd = 0.0f;break;
-        }
+    switch (key) {
+    	
+    }
 } 
 
-void mouseMove(int x, int y) { 	
-    if (xMouseState >= 0) {
-		// update deltaAngle
-		deltaAngle_x = (x - xMouseState) * 0.001f;
-		// update camera's direction
-		lx = sin(angle_x + deltaAngle_x);
-		lz = -cos(angle_x + deltaAngle_x);
+void move(){	
+	if(up && !down){
+		x += 0.03f * lx;
+		z += 0.03f * lz;
+		if(flying == 1){
+			y += 0.03f * ly;
+		}
 	}
-	if (yMouseState >= 0) {
-		// update deltaAngle
-		deltaAngle_y = (y - yMouseState) * 0.001f;
-		// update camera's direction
-		ly = sin(angle_y + deltaAngle_y);
+	if(!up && down){
+		x -= 0.03f * lx;
+		z -= 0.03f * lz;
+		if(flying == 1){
+			y -= 0.03f * ly;
+		}
 	}
+	if(left && !right){
+		x += 0.03f * lz;
+		z -= 0.03f * lx;
+	}
+	if(!left && right){
+		x -= 0.03f * lz;
+		z += 0.03f * lx;
+	}
+	if(jump){
+		if(y<3.0f){
+			y += 0.03f/(9.8*9.8/60);
+		}else if(y>=3.0f){
+			jump = 0;
+		}
+	}
+	if(!jump && y>1.8f && !flying){
+		y -= 0.03f/(9.8*9.8/60);
+	}
+	if(crouch){
+		if(y>1.0f){
+			y -= 0.03f/(9.8*9.8/60);
+		}
+	}
+}
+
+void camera(int x, int y) {
+	
+	deltaAngle_x += (x - xOrigin) * 0.001f;
+	deltaAngle_y += (y - yOrigin) * 0.001f;
+	
+	if(deltaAngle_y > 1.0f){
+		deltaAngle_y = 1.0f;
+	}else if(deltaAngle_y < -1.0f){
+		deltaAngle_y = -1.0f;
+	}
+	
+	lx = sin(angle_x + deltaAngle_x);
+	ly = -sin(angle_y + deltaAngle_y);
+	lz = -cos(angle_x + deltaAngle_x);
+
 }
 
 void mouseButton(int button, int state, int x, int y) {
-	// only start motion if the left button is pressed
-	if (button == GLUT_LEFT_BUTTON) {
+	if (button == GLUT_RIGHT_BUTTON) {
 		if (state != GLUT_UP) {
-			xMouseState = x;
-			yMouseState = y;
+
 		}
 		else if (state == GLUT_UP) {
-			angle_x += deltaAngle_x;
-			angle_y += deltaAngle_y;
-			
-			if(angle_y > 1.0f){
-				angle_y = 1.0f;
-			}else if(angle_y < -1.0f){
-				angle_y = -1.0f;
-			}
-			
-			xMouseState = -1.0f;
-			yMouseState = -1.0f;
+
 		}
 	}
-}
-
-//===========================================================================================================================
-
-int main(int argc, char **argv) {
-
-	// init GLUT and create window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(1366,768);
-	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
-
-	// register callbacks
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(screenResize);
-	glutIdleFunc(renderScene);
-
-	glutIgnoreKeyRepeat(1);
-	glutKeyboardFunc(key_press);
-	glutKeyboardUpFunc(key_release);
-	glutSpecialFunc(specKey_press);
-	glutSpecialUpFunc(specKey_release);
-
-	// here are the two new functions
-	glutMouseFunc(mouseButton);
-	glutMotionFunc(mouseMove);
-
-	// OpenGL init
-	glEnable(GL_DEPTH_TEST);
-
-	// enter GLUT event processing cycle
-	glutMainLoop();
-
-	return 1;
 }
